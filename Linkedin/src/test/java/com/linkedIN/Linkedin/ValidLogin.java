69d1e3f46e8f539118fed5c890835702f232f145
User <User@DESKTOP-K8GN66L>
2018-12-31 07:52:28 +0530

    version31

--- Linkedin/src/test/java/com/linkedIN/Linkedin/ValidLogin.java
+++ Linkedin/src/test/java/com/linkedIN/Linkedin/ValidLogin.java
@@ -1,47 +1,37 @@
 package com.linkedIN.Linkedin;
 
+import org.testng.annotations.Test;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
 
 import com.linkedIN.PageActions.LoginPageActions;
 import com.linkedIN.Resources.TestBaseSetup;
+import com.linkedIN.Util.ExtentTestManager;
 import com.linkedIN.Util.GenericUtility;
-
-
-
+import com.relevantcodes.extentreports.LogStatus;
 
 import java.io.IOException;
 
-
 import org.dom4j.DocumentException;
 
-
 public class ValidLogin extends TestBaseSetup {
-	
-	String expected="";
-	
-	
-  @Test
-  public void ValidLogin1() throws IOException, DocumentException {
-	LoginPageActions Login=new LoginPageActions(driver);
-	String Path=System.getProperty("user.dir")+"/TestData/Data.properties";
-//	String Sheet="Credentials";
-	
-String Username=GenericUtility.getData(Path, "username");
 
-String Password=GenericUtility.getData(Path, "password");
+	String expected = "";
+
+	@Test
+	public void ValidLogin1() throws IOException, DocumentException {
+		LoginPageActions Login = new LoginPageActions(driver);
+		String Path = System.getProperty("user.dir") + "/TestData/Data.properties";
+		// String Sheet="Credentials";
+
+		String Username = GenericUtility.getData(Path, "username");
+
+		String Password = GenericUtility.getData(Path, "password");
+		ExtentTestManager.getTest().log(LogStatus.INFO, "username entered is:"+Username);
+		ExtentTestManager.getTest().log(LogStatus.INFO, "password entered is:"+Password);
+		GenericUtility.VerifyLoginPageTitle(driver);
+		Login.login(Username, Password);
+		GenericUtility.waitForPageToLoad();
 	
-	Login.login(Username, Password);
-	GenericUtility.waitForPageToLoad();
-	  
-	 
-	  
-  }
- 
-
-
-
+	}
 
 }
